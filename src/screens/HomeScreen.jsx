485acfbc3a5983c2808a397e2859e55c89149ab2
Legion <pawel.kapusta@student.uj.edu.pl>
2022-05-31 17:29:37 +0200

    Add simple file button

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import styles from '../styles/HomeScreen.module.css';
 import Button from '@mui/material/Button';
 import SendIcon from '@mui/icons-material/Send';
@@ -6,15 +6,34 @@ import {useNavigate} from "react-router-dom";
 
 const HomeScreen = () => {
     const navigate = useNavigate();
+  const [data, setData] = useState();
+  const [errorData, setErrorData] = useState();
 
     const onButtonClick = () => {
         navigate("/nick");
     }
 
-    return <div className={styles.screen}>
-        <Button id={styles.button} className={styles.button} endIcon={<SendIcon/>} onClick={onButtonClick}>Rozpocznij
-            grę!</Button>
-    </div>;
+    const readFileOnUpload = (uploadedFile) =>{
+    const fileReader = new FileReader();
+    fileReader.onloadend = ()=>{
+      try{
+        setData(JSON.parse(fileReader.result));
+        setErrorData(null)
+      }catch(e){
+        setErrorData("**Not valid JSON file!**");
+      }
+    }
+    if( uploadedFile!== undefined)
+      fileReader.readAsText(uploadedFile);
+  }
+
+  return <div className={styles.screen}>
+    <Button id={styles.button} className={styles.button} endIcon={<SendIcon />} onClick={onButtonClick}>Rozpocznij grę!</Button>
+    <div className={styles.reader}>
+      <label className={styles.headerFile}>Load the world</label>
+      <input type="file" className={styles.fileReader} />
+    </div>
+  </div>;
 };
 
 export default HomeScreen;
