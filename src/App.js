3bae023d87fe6c58c8ba52e6d33e1d06219dbe21
patdem <patrycjadembinska60@gmail.com>
2022-06-11 21:55:58 +0200

    Add game store

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import {Box} from "@mui/material";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Box } from "@mui/material";
 import "./App.css";
 import HomeScreen from "./screens/HomeScreen";
 import ErrorScreen from "./screens/ErrorScreen";
@@ -10,33 +10,36 @@ import { OldWorldContextProvider } from "./store/oldWorldContext";
 import NickNavigator from "./screens/NickNavigator";
 import MainGameScreen from "./screens/MainGameScreen";
 import { LoginContextProvider } from "./store/loginContext";
+import { GameContextProvider } from "./store/gameContexts";
 
 const App = () => {
-    let theme = createTheme();
-    theme = responsiveFontSizes(theme);
-    return (
-        <BrowserRouter>
-            <LoginContextProvider>
-     <OldWorldContextProvider>
-     <ThemeProvider theme={theme}>
-                <Box
-                    sx={{
-                        display: "flex",
-                        flexDirection: "column",
-                        minHeight: "100vh",
-                    }}>
-                    <Routes>
-                        <Route exact path="/" element={<HomeScreen/>}/>
-                        <Route exact path="/nick" element={<NickNavigator/>}/>
-                        <Route exact path="/main-game" element={<MainGameScreen/>}/>
-                        <Route path="*" element={<ErrorScreen/>}/>
-                    </Routes>
-                </Box>
+  let theme = createTheme();
+  theme = responsiveFontSizes(theme);
+  return (
+    <BrowserRouter>
+      <LoginContextProvider>
+        <OldWorldContextProvider>
+          <GameContextProvider>
+            <ThemeProvider theme={theme}>
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                  minHeight: "100vh",
+                }}>
+                <Routes>
+                  <Route exact path="/" element={<HomeScreen />} />
+                  <Route exact path="/nick" element={<NickNavigator />} />
+                  <Route exact path="/main-game" element={<MainGameScreen />} />
+                  <Route path="*" element={<ErrorScreen />} />
+                </Routes>
+              </Box>
             </ThemeProvider>
-     </OldWorldContextProvider>
-     </LoginContextProvider>
-   </BrowserRouter>
-    );
+          </GameContextProvider>
+        </OldWorldContextProvider>
+      </LoginContextProvider>
+    </BrowserRouter>
+  );
 };
 
 export default App;
--- src/screens/MainGameScreen.jsx
+++ src/screens/MainGameScreen.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import styles from "../styles/MainGameScreen.module.css";
 import Equipment from "../components/Equipment";
 import HP from "../components/HP";
@@ -8,9 +8,11 @@ import Travel from "../components/Travel";
 import Nickname from "../components/Nickname";
 import Menu from "../components/Menu";
 import MainHero from "../components/MainHero";
+import { GameContext } from "../store/gameContexts";
 
 const MainGameScreen = () => {
   const nickname = localStorage.getItem("Nickname");
+  let { hp, coins } = useContext(GameContext);
 
   const buttonStyle = {
     borderRadius: 10,
@@ -23,9 +25,9 @@ const MainGameScreen = () => {
 
   return (
     <div className={styles.screen}>
-      <HP buttonStyle={buttonStyle} healthPoints={100} />
+      <HP buttonStyle={buttonStyle} healthPoints={hp} />
 
-      <Coins buttonStyle={buttonStyle} money={10} />
+      <Coins buttonStyle={buttonStyle} money={coins} />
 
       <Equipment buttonStyle={buttonStyle} />
 
