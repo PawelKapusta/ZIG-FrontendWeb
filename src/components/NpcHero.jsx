006ef4d09f204d9a604a45acde33ec55ec5d2c70
Zuzanna Miarzynska <zuzia.miarzynska@gmail.com>
2022-06-13 00:39:45 +0200

    Add exchanging

--- src/components/NpcHero.jsx
+++ src/components/NpcHero.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useContext } from "react";
-import { GameContext } from "../store/gameContexts";
+import React, {useState, useContext} from "react";
+import {GameContext} from "../store/gameContexts";
 import wizardImage from "../assets/characters/Wizard.png";
 import innkeeperImage from "../assets/characters/Innkeeper.png";
 import dragonImage from "../assets/characters/Dragon.png";
@@ -11,139 +11,279 @@ import Box from "@mui/material/Box";
 import Coins from "./Coins";
 import HP from "./HP";
 import CloseIcon from "@mui/icons-material/Close";
-import { useNavigate } from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 import deathImage from '../assets/death.jpg';
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import stylesEquipment from "../styles/Equipment.module.css";
 
 const imagesPath = {
-  Wizard: wizardImage,
-  Dragon: dragonImage,
-  Innkeeper: innkeeperImage,
-  Sheep: sheepImage,
+    Wizard: wizardImage,
+    Dragon: dragonImage,
+    Innkeeper: innkeeperImage,
+    Sheep: sheepImage,
 };
 
 const style = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: 700,
-  bgcolor: "background.paper",
-  border: "2px solid #000",
-  borderRadius: 4,
-  boxShadow: 24,
-  pt: 2,
-  px: 4,
-  pb: 3,
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 700,
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    borderRadius: 4,
+    boxShadow: 24,
+    pt: 2,
+    px: 4,
+    pb: 3,
 };
 
-const NPCHero = ({ name = "", items = [], attributes = [] }) => {
-  const currentItems = useState(items);
-  const navigate = useNavigate();
-  const [open, setOpen] = React.useState(false);
-  const [deathOpen, setDeathOpen] = React.useState(false);
-  const { hp, setHp } = useContext(GameContext);
+const NPCHero = ({name = "", NPCitems = [], attributes = []}) => {
+    const currentItems = useState(NPCitems);
+    const navigate = useNavigate();
+    const [open, setOpen] = React.useState(false);
+    const [deathOpen, setDeathOpen] = React.useState(false);
+    const [exchangeOpen, setExchangeOpen] = React.useState(false);
+    const {
+        hp,
+        setHp,
+        items,
+        currentLocation,
+        exchanging,
+        setExchanging,
+        exchanged,
+        setExchanged
+    } = useContext(GameContext);
+    let myCharacterItems = currentLocation[0]
+    const handleClose = () => {
+        setOpen(false);
+    };
 
-  const handleClose = () => {
-    setOpen(false);
-  };
+    const handleDeathClose = () => {
+        setDeathOpen(false);
+    };
 
-  const handleDeathClose = () => {
-    setDeathOpen(false);
-  };
+    const handleExchangeClose = () => {
+        setExchangeOpen(false);
+    };
+    const getImagePathByCharacterName = name => {
+        return imagesPath[name];
+    };
 
-  const getImagePathByCharacterName = name => {
-    return imagesPath[name];
-  };
+    const showOptions = () => {
+        setOpen(true);
+    };
 
-  const showOptions = () => {
-    setOpen(true);
-  };
+    const onFightClick = () => {
+        const hpAfterFight = hp - Math.round((2 / 3) * attributes?.HP);
+        if (hpAfterFight <= 0) {
+            setHp(0);
+            setDeathOpen(true);
 
-  const onFightClick = () => {
-    const hpAfterFight = hp - Math.round((2 / 3) * attributes?.HP);
-    if (hpAfterFight <= 0 ) {
-      setHp(0);
-      setDeathOpen(true);
+        } else {
+            setHp(hpAfterFight);
+            setOpen(false);
+        }
+    };
 
-    } else {
-      setHp(hpAfterFight);
-      setOpen(false);
+    const onExchangeClick = () => {
+        setOpen(false);
+        setExchangeOpen(true);
+
+    };
+
+    const onMoveToMenu = () => {
+        navigate("/");
     }
-  };
 
-  const onExchangeClick = () => {};
 
-  const onMoveToMenu = () => {
-    navigate("/");
-  }
+    function toYoursExchange(event, itemName) {
+        for (var i = 0; i < items.length; i++) {
+            if (items[i].Name === itemName) {
+                setExchanging(items[i]);
+                break;
+            }
+        }
 
-  console.log(items);
-  return (
-    <div>
-      <div className={styles.npcHero} onClick={showOptions}>
-        <img id={styles.NpcHero} src={getImagePathByCharacterName(name)} alt="NpcHero" />
-      </div>
-      <Modal
-        hideBackdrop
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="child-modal-title"
-        aria-describedby="child-modal-description"
-      >
-        <Box sx={{ ...style }}>
-          <h2 id="child-modal-title">{name}</h2>
-          <div className={styles.npcAttributes}>
-            <HP isShowed healthPoints={attributes["HP"]} buttonStyle={{ height: 10 }} />
-            {attributes["Money"] ? (
-              <Coins isShowed money={attributes["Money"]} buttonStyle={{ width: 10 }} />
-            ) : (
-              ""
-            )}
-          </div>
-          <div id={styles.npcHeroButtons}>
-            <Button id={styles.npcButton} className={styles.npcButton} onClick={onFightClick}>
-              Fight
-            </Button>
-            {items?.length > 0 ? (
-              <Button id={styles.npcButton} className={styles.npcButton} onClick={onExchangeClick}>
-                Exchange
-              </Button>
-            ) : (
-              ""
-            )}
-          </div>
-          <Button
-            onClick={handleClose}
-            id={styles.npcHeroButtonClose}
-            endIcon={<CloseIcon style={{ color: "black" }} />}
-          />
-        </Box>
-      </Modal>
-      <Modal
-       hideBackdrop
-       open={deathOpen}
-       onClose={handleDeathClose}
-       aria-labelledby="child-modal-title"
-       aria-describedby="child-modal-description"
-      >
-        <Box sx={{ ...style }}>
-            <h2 id="child-modal-title">Death</h2>
-            <img id={styles.npcHeroDeath} src={deathImage} alt="NpcHero" />
-          <h5 id={styles.npcHeroDeathText}> You lost to {name} :( </h5>
-          <Button
-           onClick={handleDeathClose}
-           id={styles.npcHeroButtonClose}
-           endIcon={<CloseIcon style={{ color: "black" }} />}
-          />
-          <div id={styles.npcHeroButtons}>
-            <Button id={styles.npcButton} className={styles.npcButton} onClick={onMoveToMenu}>
-              Go to Menu
-            </Button>
-          </div>
-        </Box>
-      </Modal>
-    </div>
-  );
+    }
+
+    function toNPCExchange(event, itemName) {
+        for (var i = 0; i < NPCitems.length; i++) {
+            if (NPCitems[i].Name === itemName) {
+                setExchanged(NPCitems[i]);
+                break;
+            }
+        }
+
+    }
+
+    const makeExchange = () => {
+
+        for (var i = 0; i < items.length; i++) {
+            if (items[i].Name === exchanging.Name) {
+                items.splice(i, 1);
+                items.push(exchanged);
+            }
+        }
+        for (var i = 0; i < NPCitems.length; i++) {
+            if (NPCitems[i].Name === exchanged.Name) {
+                NPCitems.splice(i, 1);
+                NPCitems.push(exchanging);
+            }
+        }
+        handleExchangeClose();
+
+
+    }
+
+    console.log(items);
+    return (
+        <div>
+            <div className={styles.npcHero} onClick={showOptions}>
+                <img id={styles.NpcHero} src={getImagePathByCharacterName(name)} alt="NpcHero"/>
+            </div>
+            <Modal
+                hideBackdrop
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="child-modal-title"
+                aria-describedby="child-modal-description"
+            >
+                <Box sx={{...style}}>
+                    <h2 id="child-modal-title">{name}</h2>
+                    <div className={styles.npcAttributes}>
+                        <HP isShowed healthPoints={attributes["HP"]} buttonStyle={{height: 10}}/>
+                        {attributes["Money"] ? (
+                            <Coins isShowed money={attributes["Money"]} buttonStyle={{width: 10}}/>
+                        ) : (
+                            ""
+                        )}
+                    </div>
+                    <div id={styles.npcHeroButtons}>
+                        <Button id={styles.npcButton} className={styles.npcButton} onClick={onFightClick}>
+                            Fight
+                        </Button>
+                        {items?.length > 0 ? (
+                            <Button id={styles.npcButton} className={styles.npcButton} onClick={onExchangeClick}>
+                                Exchange
+                            </Button>
+
+                        ) : (
+                            ""
+                        )}
+                    </div>
+                    <Button
+                        onClick={handleClose}
+                        id={styles.npcHeroButtonClose}
+                        endIcon={<CloseIcon style={{color: "black"}}/>}
+                    />
+                </Box>
+            </Modal>
+            <Modal
+                hideBackdrop
+                open={deathOpen}
+                onClose={handleDeathClose}
+                aria-labelledby="child-modal-title"
+                aria-describedby="child-modal-description"
+            >
+                <Box sx={{...style}}>
+                    <h2 id="child-modal-title">Death</h2>
+                    <img id={styles.npcHeroDeath} src={deathImage} alt="NpcHero"/>
+                    <h5 id={styles.npcHeroDeathText}> You lost to {name} :( </h5>
+                    <Button
+                        onClick={handleDeathClose}
+                        id={styles.npcHeroButtonClose}
+                        endIcon={<CloseIcon style={{color: "black"}}/>}
+                    />
+                    <div id={styles.npcHeroButtons}>
+                        <Button id={styles.npcButton} className={styles.npcButton} onClick={onMoveToMenu}>
+                            Go to Menu
+                        </Button>
+                    </div>
+                </Box>
+            </Modal>
+
+            <Modal
+                hideBackdrop
+                open={exchangeOpen}
+                onClose={handleExchangeClose}
+                aria-labelledby="child-modal-title"
+                aria-describedby="child-modal-description"
+            >
+                <Box sx={{...style}}>
+                    <h2 id="child-modal-title">Exchange</h2>
+                    <Button
+                        onClick={handleExchangeClose}
+                        id={styles.npcHeroButtonClose}
+                        endIcon={<CloseIcon style={{color: "black"}}/>}
+                    />
+                    <h2>Yours items</h2>
+                    <Grid container spacing={3}>
+                        {items.map(item => (
+                            <Grid item key={item.Name} xs={4} sm={4} md={4} lg={4}>
+                                <Paper>
+                                    {" "}
+                                    {item.Name}
+                                    <img
+                                        className={stylesEquipment.itemPhoto}
+                                        src={require("../assets/items/" + item.Name + ".png")}
+                                        alt=""
+                                    />
+                                </Paper>
+                                <Paper>Value: {item.Attributes.Value}</Paper>
+                                <Paper>Nutritional: {item.Attributes.NutritionalValue}</Paper>
+                                <Paper>IsPoison: {item.Attributes.IsPoison}</Paper>
+                                <Button
+                                    onClick={e => {
+                                        toYoursExchange(e, item.Name);
+                                    }}
+                                >
+                                    {" "}
+                                    Exchange
+                                </Button>
+                            </Grid>
+                        ))}
+                    </Grid>
+
+                    <h2>{name}'s items</h2>
+                    <Grid container spacing={3}>
+                        {NPCitems.map(item => (
+                            <Grid item key={item.Name} xs={4} sm={4} md={4} lg={4}>
+                                <Paper>
+                                    {" "}
+                                    {item.Name}
+                                    <img
+                                        className={stylesEquipment.itemPhoto}
+                                        src={require("../assets/items/" + item.Name + ".png")}
+                                        alt=""
+                                    />
+                                </Paper>
+                                <Paper>Value: {item.Attributes.Value}</Paper>
+                                <Paper>Nutritional: {item.Attributes.NutritionalValue}</Paper>
+                                <Paper>IsPoison: {item.Attributes.IsPoison}</Paper>
+                                <Button
+                                    onClick={e => {
+                                        toNPCExchange(e, item.Name);
+                                    }}
+                                >
+                                    {" "}
+                                    Exchange
+                                </Button>
+                            </Grid>
+                        ))}
+                    </Grid>
+
+                    <div id={styles.npcHeroButtons}>
+
+                        <Button id={styles.npcButton} className={styles.npcButton} onClick={makeExchange}>
+                            Make Exchange
+                        </Button>
+                    </div>
+                </Box>
+            </Modal>
+        </div>
+    );
 };
 
 export default NPCHero;
--- src/store/gameContexts.jsx
+++ src/store/gameContexts.jsx
@@ -1,273 +1,276 @@
-import React, { useState, createContext } from "react";
+import React, {useState, createContext} from "react";
 import imageUrl from "../assets/landscape.jpg";
 
 export const GameContext = createContext();
 
-export const GameContextProvider = ({ children }) => {
-  const [hp, setHp] = useState(100);
-  const [coins, setCoins] = useState(10);
-  const [locations, setLocations] = useState(dragonStoryLocations);
-  const [currentLocation, setCurrentLocation] = useState(dragonStoryLocations[0]);
-  const [backgroundImage, setBackgroundImage] = useState(imageUrl);
-  const [items, setItems] = useState(dragonStoryLocations[0]["Characters"][0]["Items"]);
-  const [exchange, setExchange] = useState(dragonStoryLocations[0]["Characters"]);
+export const GameContextProvider = ({children}) => {
+    const [hp, setHp] = useState(100);
+    const [coins, setCoins] = useState(10);
+    const [locations, setLocations] = useState(dragonStoryLocations);
+    const [currentLocation, setCurrentLocation] = useState(dragonStoryLocations[0]);
+    const [backgroundImage, setBackgroundImage] = useState(imageUrl);
+    const [items, setItems] = useState(dragonStoryLocations[0]["Characters"][0]["Items"]);
+    const [exchanging, setExchanging] = useState(null);
+    const [exchanged, setExchanged] = useState(null);
 
-  const providerValue = {
-    hp,
-    setHp,
-    coins,
-    setCoins,
-    locations,
-    setLocations,
-    currentLocation,
-    setCurrentLocation,
-    backgroundImage,
-    setBackgroundImage,
-    items,
-    setItems,
-    exchange,
-    setExchange,
-  };
+    const providerValue = {
+        hp,
+        setHp,
+        coins,
+        setCoins,
+        locations,
+        setLocations,
+        currentLocation,
+        setCurrentLocation,
+        backgroundImage,
+        setBackgroundImage,
+        items,
+        setItems,
+        exchanging,
+        setExchanging,
+        exchanged,
+        setExchanged,
+    };
 
-  return <GameContext.Provider value={providerValue}>{children}</GameContext.Provider>;
+    return <GameContext.Provider value={providerValue}>{children}</GameContext.Provider>;
 };
 
 const dragonStoryLocations = [
-  {
-    Id: "0",
-    Name: "Road",
-    Characters: [
-      {
-        Name: "Main_hero",
-        Items: [
-          {
-            Name: "Sword",
-            Attributes: {
-              Value: 1000,
+    {
+        Id: "0",
+        Name: "Road",
+        Characters: [
+            {
+                Name: "Main_hero",
+                Items: [
+                    {
+                        Name: "Sword",
+                        Attributes: {
+                            Value: 1000,
+                        },
+                    },
+                    {
+                        Name: "Elixir",
+                        Attributes: {
+                            Value: 10,
+                            NutritionalValue: 1000,
+                        },
+                    },
+                    {
+                        Name: "Dragon_egg",
+                        Attributes: {
+                            Value: 4000,
+                        },
+                    },
+                    {
+                        Name: "Elixir",
+                        Attributes: {
+                            Value: 10,
+                            NutritionalValue: 1000,
+                        },
+                    },
+                ],
+                Attributes: {
+                    HP: 100,
+                    Money: 10,
+                },
             },
-          },
-          {
-            Name: "Elixir",
-            Attributes: {
-              Value: 10,
-              NutritionalValue: 1000,
-            },
-          },
-          {
-            Name: "Dragon_egg",
-            Attributes: {
-              Value: 4000,
-            },
-          },
-          {
-            Name: "Elixir",
-            Attributes: {
-              Value: 10,
-              NutritionalValue: 1000,
-            },
-          },
         ],
-        Attributes: {
-          HP: 100,
-          Money: 10,
-        },
-      },
-    ],
-    Items: [],
-    Connections: [
-      {
-        Destination: "1",
-      },
-      {
-        Destination: "2",
-      },
-      {
-        Destination: "3",
-      },
-      {
-        Destination: "4",
-      },
-      {
-        Destination: "5",
-      },
-    ],
-  },
-  {
-    Id: "5",
-    Name: "Wizards_hut",
-    Attributes: {
-      IsPrivate: true,
+        Items: [],
+        Connections: [
+            {
+                Destination: "1",
+            },
+            {
+                Destination: "2",
+            },
+            {
+                Destination: "3",
+            },
+            {
+                Destination: "4",
+            },
+            {
+                Destination: "5",
+            },
+        ],
     },
-    Characters: [
-      {
-        Name: "Wizard",
+    {
+        Id: "5",
+        Name: "Wizards_hut",
         Attributes: {
-          HP: 200,
-          Money: 100,
-          IsAuthority: true,
+            IsPrivate: true,
         },
-      },
-    ],
-    Items: [
-      {
-        Name: "Elixir",
-        Attributes: {
-          Value: 10,
-          NutritionalValue: 1000,
-        },
-      },
-    ],
-    Connections: [
-      {
-        Destination: "0",
-      },
-    ],
-  },
-  {
-    Id: "6",
-    Name: "Dragons_lair",
-    Characters: [
-      {
-        Name: "Dragon",
-        Attributes: {
-          HP: 1000,
-        },
-      },
-    ],
-    Items: [
-      {
-        Name: "Dragon_egg",
-        Attributes: {
-          Value: 4000,
-        },
-      },
-    ],
-    Connections: [
-      {
-        Destination: "1",
-      },
-    ],
-  },
-  {
-    Id: "1",
-    Name: "Forest",
-    Characters: [],
-    Items: [
-      {
-        Name: "Herbs",
-        Attributes: {
-          Value: 30,
-          IsPoison: true,
-        },
-      },
-    ],
-    Connections: [
-      {
-        Destination: "0",
-      },
-      {
-        Destination: "6",
-      },
-    ],
-  },
-  {
-    Id: "3",
-    Name: "Pasture",
-    Characters: [
-      {
-        Name: "Sheep",
-        Attributes: {
-          HP: 10,
-          NutritionalValue: 20,
-        },
-      },
-    ],
-    Items: [],
-    Connections: [
-      {
-        Destination: "0",
-      },
-      {
-        Destination: "4",
-      },
-    ],
-  },
-  {
-    Id: "4",
-    Name: "Village",
-    Items: [
-      {
-        Name: "Well",
-        Attributes: {
-          Value: 10,
-        },
-      },
-    ],
-    Connections: [
-      {
-        Destination: "0",
-      },
-      {
-        Destination: "3",
-      },
-    ],
-  },
-  {
-    Id: "2",
-    Name: "Inn",
-    Characters: [
-      {
-        Name: "Innkeeper",
-        Items: [
-          {
-            Name: "Poison",
-            Attributes: {
-              Value: 1000,
-              IsPoison: true,
+        Characters: [
+            {
+                Name: "Wizard",
+                Attributes: {
+                    HP: 200,
+                    Money: 100,
+                    IsAuthority: true,
+                },
             },
-          },
         ],
-        Attributes: {
-          HP: 100,
-          Money: 1200,
-        },
-      },
-      {
-        Name: "Drunkard",
         Items: [
-          {
-            Name: "Alcohol",
-            Attributes: {
-              Value: 3,
+            {
+                Name: "Elixir",
+                Attributes: {
+                    Value: 10,
+                    NutritionalValue: 1000,
+                },
             },
-          },
         ],
-        Attributes: {
-          HP: 100,
-          Money: 0,
-          IsTroublemaker: true,
-        },
-      },
-    ],
-    Items: [],
-    Connections: [
-      {
-        Destination: "7",
-      },
-      {
-        Destination: "3",
-      },
-    ],
-  },
-  {
-    Id: "7",
-    Name: "Prison",
-    Characters: [],
-    Items: [],
-    Connections: [
-      {
-        Destination: "0",
-      },
-    ],
-  },
+        Connections: [
+            {
+                Destination: "0",
+            },
+        ],
+    },
+    {
+        Id: "6",
+        Name: "Dragons_lair",
+        Characters: [
+            {
+                Name: "Dragon",
+                Attributes: {
+                    HP: 1000,
+                },
+            },
+        ],
+        Items: [
+            {
+                Name: "Dragon_egg",
+                Attributes: {
+                    Value: 4000,
+                },
+            },
+        ],
+        Connections: [
+            {
+                Destination: "1",
+            },
+        ],
+    },
+    {
+        Id: "1",
+        Name: "Forest",
+        Characters: [],
+        Items: [
+            {
+                Name: "Herbs",
+                Attributes: {
+                    Value: 30,
+                    IsPoison: true,
+                },
+            },
+        ],
+        Connections: [
+            {
+                Destination: "0",
+            },
+            {
+                Destination: "6",
+            },
+        ],
+    },
+    {
+        Id: "3",
+        Name: "Pasture",
+        Characters: [
+            {
+                Name: "Sheep",
+                Attributes: {
+                    HP: 10,
+                    NutritionalValue: 20,
+                },
+            },
+        ],
+        Items: [],
+        Connections: [
+            {
+                Destination: "0",
+            },
+            {
+                Destination: "4",
+            },
+        ],
+    },
+    {
+        Id: "4",
+        Name: "Village",
+        Items: [
+            {
+                Name: "Well",
+                Attributes: {
+                    Value: 10,
+                },
+            },
+        ],
+        Connections: [
+            {
+                Destination: "0",
+            },
+            {
+                Destination: "3",
+            },
+        ],
+    },
+    {
+        Id: "2",
+        Name: "Inn",
+        Characters: [
+            {
+                Name: "Innkeeper",
+                Items: [
+                    {
+                        Name: "Poison",
+                        Attributes: {
+                            Value: 1000,
+                            IsPoison: true,
+                        },
+                    },
+                ],
+                Attributes: {
+                    HP: 100,
+                    Money: 1200,
+                },
+            },
+            {
+                Name: "Drunkard",
+                Items: [
+                    {
+                        Name: "Alcohol",
+                        Attributes: {
+                            Value: 3,
+                        },
+                    },
+                ],
+                Attributes: {
+                    HP: 100,
+                    Money: 0,
+                    IsTroublemaker: true,
+                },
+            },
+        ],
+        Items: [],
+        Connections: [
+            {
+                Destination: "7",
+            },
+            {
+                Destination: "3",
+            },
+        ],
+    },
+    {
+        Id: "7",
+        Name: "Prison",
+        Characters: [],
+        Items: [],
+        Connections: [
+            {
+                Destination: "0",
+            },
+        ],
+    },
 ];
