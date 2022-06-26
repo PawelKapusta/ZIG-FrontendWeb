5e0744cea69b44dbf0fba927e37f7249ca9ee947
Zuzanna Miarzynska <zuzia.miarzynska@gmail.com>
2022-06-26 19:05:09 +0200

    Add picking app to backend

--- src/components/Pickup.jsx
+++ src/components/Pickup.jsx
@@ -1,86 +1,87 @@
-// import React, { useContext, useState } from "react";
-// import { GameContext } from "../store/gameContexts";
-// import { Button } from "@mui/material";
-// import styles from "../styles/MainGameScreen.module.css";
-// import Dialog from "@mui/material/Dialog";
-// import DialogTitle from "@mui/material/DialogTitle";
-// import DialogContent from "@mui/material/DialogContent";
-// import Grid from "@mui/material/Grid";
-// import Paper from "@mui/material/Paper";
-// import stylesEquipment from "../styles/Equipment.module.css";
+import React, { useContext, useState } from "react";
+import { GameContext } from "../store/gameContexts";
+import { Button } from "@mui/material";
+import styles from "../styles/MainGameScreen.module.css";
+import Dialog from "@mui/material/Dialog";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import stylesEquipment from "../styles/Equipment.module.css";
+import {getNewStateGame} from "../api/client";
 
 const ModalPickup = props => {
-  // const buttonStyle = props.buttonStyle;
+  const buttonStyle = props.buttonStyle;
 
-  // let { items, currentLocation } = useContext(GameContext);
-  // let availableItems = currentLocation.hasOwnProperty("Items") ? currentLocation.Items : [];
+  let { items, currentLocation, game, setGame, pickingProductions, setPickingProductions } = useContext(GameContext);
+  let availableItems = currentLocation?.hasOwnProperty("Items") ? currentLocation.Items : [];
 
-  // function seeAvailableItems() {
-  //   handleClickOpen();
-  // }
+  function seeAvailableItems() {
+    handleClickOpen();
+  }
 
-  // const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
 
-  // const handleClickOpen = () => {
-  //   setOpen(true);
-  // };
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
 
-  // const handleClose = () => {
-  //   setOpen(false);
-  // };
+  const handleClose = () => {
+    setOpen(false);
+  };
 
-  // function toGet(event, itemName) {
-  //   for (var i = 0; i < availableItems.length; i++) {
-  //     if (availableItems[i].Name === itemName) {
-  //       items.push(availableItems[i]);
-  //       availableItems.splice(i, 1);
-  //       break;
-  //     }
-  //   }
-  //   handleClose();
-  // }
+  async function onAddItemToEquipment(event, item) {
+    const body = {
+      world: game.world,
+      production: item.production,
+      variant: item.variant,
+    };
+    console.error(body);
+    const response = await getNewStateGame(body);
+    setGame(response);
+    handleClose();
+  }
 
   return (
     <div>
-      {/* <Button
+      {<Button
         className={styles.buttonAvailableItems}
         style={buttonStyle}
         onClick={seeAvailableItems}
       >
         Available items
-      </Button>
-
-      <Dialog open={open} onClose={handleClose}>
+      </Button>}
+      {<Dialog open={open} onClose={handleClose}>
         <DialogTitle>You can possess those items </DialogTitle>
         <DialogContent>
           <Grid container spacing={3}>
-            {availableItems.map(item => (
-              <Grid item key={item.Name} xs={4} sm={4} md={4} lg={4}>
+            {pickingProductions?.map(item => (
+              <Grid item key={item.variant[2].WorldNodeName} xs={4} sm={4} md={4} lg={4}>
                 <Paper>
                   {" "}
                   {item.Name}
                   <img
                     className={stylesEquipment.itemPhoto}
-                    src={require("../assets/items/" + item.Name + ".png")}
+                    src={require("../assets/items/" + item.variant[2].WorldNodeName + ".png")}
                     alt=""
                   />
                 </Paper>
-                <Paper>Value: {item.Attributes.Value}</Paper>
-                <Paper>Nutritional: {item.Attributes.NutritionalValue.valueOf()}</Paper>
-                <Paper>IsPoison: {item.Attributes.IsPoison.toString()}</Paper>
+                <Paper>Value: {item.variant[2].WorldNodeAttr.Value}</Paper>
+                {item.variant[2].WorldNodeAttr.NutritionalValue?.valueOf() > 0 ? <Paper>Nutrition: {item.variant[2].WorldNodeAttr.NutritionalValue?.valueOf()} </Paper>:null}
+                {item.variant[2].WorldNodeAttr.IsPoison?.toString() == 'true'? <Paper>IsPoison</Paper>:null}
                 <Button
                   onClick={e => {
-                    toGet(e, item.Name);
+                    onAddItemToEquipment(e, item);
                   }}
                 >
                   {" "}
-                  Get
+                  Take item
                 </Button>
               </Grid>
             ))}
           </Grid>
         </DialogContent>
-      </Dialog> */}
+      </Dialog> }
     </div>
   );
 };
